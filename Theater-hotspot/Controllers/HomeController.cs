9f5bd06c50136a335a580af99ce94f0f5c3e1bd8
Amber van der Riet <110250@st-maartenscollege.nl>
2022-04-06 09:55:32 +0200

    Databases terugkoppelen (begin)

--- Theater-hotspot/Controllers/HomeController.cs
+++ Theater-hotspot/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MySql.Data;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -6,6 +7,8 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Theater_hotspot.Models;
+using Theater_hotspot.Database;
+using Theater_hotspot.Databases;
 
 namespace Theater_hotspot.Controllers
 {
@@ -20,8 +23,20 @@ namespace Theater_hotspot.Controllers
 
         public IActionResult Index()
         {
-            return View();
-        }
+           
+            var rows = DatabaseConnector.GetRows("select * from product");
+            var products = GetAllProducts();
+
+            List<string> names = new List<string>();
+
+            foreach(var row in rows)
+            {
+                names.Add(row["naam"].ToString());
+            }
+               
+            return View(names);
+            return View(products);
+        }  
 
         public IActionResult Privacy()
         {
@@ -63,6 +78,29 @@ namespace Theater_hotspot.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public List<Product> GetAllProducts()
+        {
+            // alle producten ophalen uit de database
+            var rows = DatabaseConnector.GetRows("select * from product");
+
+            // lijst maken om alle producten in te stoppen
+            List<Product> products = new List<Product>();
+
+            foreach (var row in rows)
+            {
+                // Voor elke rij maken we nu een product
+                Product p = new Product();
+                p.Naam = row["naam"].ToString();
+                p.Prijs = row["prijs"].ToString();
+                p.Beschikbaarheid = Convert.ToInt32(row["beschikbaarheid"]);
+                p.Id = Convert.ToInt32(row["id"]);
+
+                // en dat product voegen we toe aan de lijst met producten
+                products.Add(p);
+            }
+
+            return products;
         }
     }
 }
